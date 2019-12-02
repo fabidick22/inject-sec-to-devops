2aaa7ad4d8622d0ab36af989d73b7a237800ffd5
fabidick22 <fabidick22@hotmail.es>
2019-12-01 21:27:06 -0500

    First app in flask

---
+++ app/__init__.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from .app import app
+from config.development import DevelopmentConfig
+from config.staging import StagingConfig
+from config.production import ProductionConfig
+
+config_values = {
+    "dev": DevelopmentConfig,
+    "stg": StagingConfig,
+    "prod": ProductionConfig,
+}
+
+def create_app(config_name='dev'):
+    """
+    Create App
+    :param config_name:
+    :return:
+    """
+    app_api = app
+    load_config(app_api, config_name)
+
+    return app_api
+
+
+def load_config(app_config: Flask, config_mode) -> None:
+    """
+    Load the application's config
+    :param (flask.app.Flask) app_config: The application instance Flask that'll be running
+    :param config_mode: Config mode (Allowed: [dev, stg, prod])
+    :return:
+    """
+    if config_mode in config_values:
+        print("Config mode: {}".format(config_mode))
+        app_config.config.from_object(config_values.get(config_mode))
+    else:
+        raise Exception("Configuration Mode Not Supported")
