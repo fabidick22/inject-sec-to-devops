5053579e5ce09b2484d84aea24790ceea468b86a
fabidick22 <fabidick22@hotmail.es>
2019-12-08 02:17:21 -0500

    Bad code

--- app/resources/bad_code.py
+++ app/resources/bad_code.py
@@ -1,5 +1,19 @@
 from flask_restful import Resource
 from flask.app import Response
+from flask import request
+# import os
+
+
+class Calc(Resource):
+    def get(self):
+        return {"message": "Method not supported!"}, 501
+
+    def post(self):
+        json_data = request.get_json(True)
+        print(json_data)
+        value_a = json_data.get("a", None)
+        value_b = json_data.get("b", None)
+        return eval("{} + {}".format(value_a, value_b))
 
 
 class Hello(Resource):
