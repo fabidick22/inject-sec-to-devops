59c7cafeb2187cf854739d762fa3d0b383d40987
fabidick22 <fabidick22@hotmail.es>
2019-12-08 22:52:35 -0500

    Add bad code

--- app/resources/bad_code.py
+++ app/resources/bad_code.py
@@ -1,7 +1,7 @@
 from flask_restful import Resource
 from flask.app import Response
 from flask import request
-# import os
+from config.development import DevelopmentConfig
 
 
 class Calc(Resource):
@@ -13,12 +13,18 @@ class Calc(Resource):
         print(json_data)
         value_a = json_data.get("a", None)
         value_b = json_data.get("b", None)
-        return eval("{} + {}".format(value_a, value_b))
+        # return eval("{} + {}".format(value_a, value_b))
+        return value_a + value_b
 
 
 class Hello(Resource):
     def get(self):
         return {"message": "hello world from flask!"}, 200
+
+    def post(self):
+        json_data = request.get_json(True)
+        str_format = json_data.get("text", None)
+        return str_format.format(details=DevelopmentConfig())
 
 
 class Base(Resource):
